eec8e429b895d2eae93e9c45ab7dd031b6f3c0ea
wanhuan <2574345757@qq.com>
2019-10-17 18:40:38 +0800

    学习 vue之 vuex-demo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome WH!" wh="哈哈哈哈"/>
+    <p>count: {{count}}</p>
+    <button @click="countADD">点击</button>
   </div>
 </template>
 
@@ -12,7 +14,18 @@ export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  computed: {
+    count(){
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    countADD(){
+      const v=100;
+      this.$store.commit('countAdd',v)
+    }
+  },
 }
 </script>
 
@@ -24,5 +37,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button{
+  border:1px red(101, 179, 101) solid;
+  padding:10px;
+  background:rgb(65, 184, 131);
+  border-radius: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    count2: 100,
+  },
+  mutations: {
+    countAdd(state,v) {
+      state.count = v;
+    },
+    countDel(state){
+      state.count2--;
+    },
+    count2Add(state){
+      state.count2++;
+    }
+  }
+})
+
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
